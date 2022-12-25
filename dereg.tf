8e8fe4f0dcecf0758cefc4d4edbee15804831f6f
lifeofguenter <gunter@grodotzki.com>
2022-12-25 09:37:48 +0100

    force re-trigger

--- dereg.tf
+++ dereg.tf
@@ -1,4 +1,8 @@
 resource "time_sleep" "grace" {
   depends_on       = [docker_container.main]
   destroy_duration = "${var.deregistration_delay}s"
+
+  triggers = {
+    container_name = docker_container.main.name
+  }
 }
